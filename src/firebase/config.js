7d08e20caab88caa456818d0e4c965706dad7649
AndresSalomon1990 <a.salomon.1990@gmail.com>
2022-03-15 16:53:35 -0300

    Setup Firebase backend and config files. Update README

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDU0BdA15xhHG7RZm-zv1x1RoPjGH3SNZI',
+  authDomain: 'project-management-site-827a9.firebaseapp.com',
+  projectId: 'project-management-site-827a9',
+  storageBucket: 'project-management-site-827a9.appspot.com',
+  messagingSenderId: '344043533977',
+  appId: '1:344043533977:web:fe677ed3a0bd4c3c3d6a08',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
