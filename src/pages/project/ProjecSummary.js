63140825009b48e91e3e5dfc007ce1eabee76a2a
AndresSalomon1990 <a.salomon.1990@gmail.com>
2022-03-25 20:12:50 -0300

    Create button to delete completed projects

--- src/pages/project/ProjecSummary.js
+++ src/pages/project/ProjecSummary.js
@@ -1,21 +1,41 @@
 import Avatar from '../../components/avatar/Avatar';
+import { useFirestore } from '../../hooks/useFirestore';
+import { useAuthContext } from '../../hooks/useAuthContext';
+import { useHistory } from 'react-router-dom';
 
 export default function ProjecSummary({ project }) {
+  const { deleteDocument } = useFirestore('projects');
+  const { user } = useAuthContext();
+  const history = useHistory();
+  
+  const handleClick = () => {
+    deleteDocument(project.id);
+    history.push('/');
+  };
+
   return (
-    <div className='project-summary'>
-      <h2 className='page-title'>{project.name}</h2>
-      <p className='due-date'>
-        Project due by {project.dueDate.toDate().toDateString()}
-      </p>
-      <p className='details'>{project.details}</p>
-      <h4>The project is assign to:</h4>
-      <div className='assigned-users'>
-        {project.assignedUsersList.map((user) => (
-          <div key={user.id}>
-            <Avatar src={user.photoURL} />
-          </div>
-        ))}
+    <div>
+      <div className='project-summary'>
+        <h2 className='page-title'>{project.name}</h2>
+        <p>By {project.createdBy.displayName}</p>
+        <p className='due-date'>
+          Project due by {project.dueDate.toDate().toDateString()}
+        </p>
+        <p className='details'>{project.details}</p>
+        <h4>The project is assign to:</h4>
+        <div className='assigned-users'>
+          {project.assignedUsersList.map((user) => (
+            <div key={user.id}>
+              <Avatar src={user.photoURL} />
+            </div>
+          ))}
+        </div>
       </div>
+      {user.uid === project.createdBy.id && (
+        <button className='btn' onClick={handleClick}>
+          Mark as complete
+        </button>
+      )}
     </div>
   );
 }
