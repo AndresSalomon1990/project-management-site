acea0f1cffc1556c3cbe672876fd4fa5435ad20b
AndresSalomon1990 <a.salomon.1990@gmail.com>
2022-03-25 23:58:22 -0300

    Add filter project logic to Dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import ProjectList from '../../components/project-list/ProjectList';
 import { useCollection } from '../../hooks/useCollection';
 import ProjectFilter from './ProjectFilter';
 import { useState } from 'react';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 // styles
 import './Dashboard.css';
@@ -9,10 +10,33 @@ import './Dashboard.css';
 export default function Dashboard() {
   const { documents, error } = useCollection('projects');
   const [currentFilter, setCurrentFilter] = useState('all');
+  const { user } = useAuthContext();
 
   const changeFilter = (newFilter) => {
     setCurrentFilter(newFilter);
   };
+
+  const projects = documents
+    ? documents.filter((document) => {
+        switch (currentFilter) {
+          case 'all':
+            return true;
+          case 'mine':
+            let assignToMe = false;
+            document.assignedUsersList.forEach((u) => {
+              if (user.uid === u.id) assignToMe = true;
+            });
+            return assignToMe;
+          case 'development':
+          case 'design':
+          case 'sales':
+          case 'marketing':
+            return document.category === currentFilter;
+          default:
+            return true;
+        }
+      })
+    : null;
 
   return (
     <div>
@@ -24,7 +48,7 @@ export default function Dashboard() {
           changeFilter={changeFilter}
         />
       )}
-      {documents && <ProjectList projects={documents} />}
+      {projects && <ProjectList projects={projects} />}
     </div>
   );
 }
